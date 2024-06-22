7ab1c52a3fa6e7c4b6be1b7f2aa0a03dec9a3283
jamesLaurino <petersthomas90@gmail.com>
2024-06-22 16:30:55 +0200

    Ajout du slide Panel

--- src/main/java/org/example/firstappfx/view/PersonViewController.java
+++ src/main/java/org/example/firstappfx/view/PersonViewController.java
@@ -1,14 +1,21 @@
 package org.example.firstappfx.view;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import javafx.util.converter.NumberStringConverter;
 import org.example.firstappfx.service.NavigationManager;
 import org.example.firstappfx.viewmodel.PersonViewModel;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PersonViewController
+
+public class PersonViewController implements Initializable
 {
     @FXML
     TextField firstNameField;
@@ -19,10 +26,13 @@ public class PersonViewController
     @FXML
     TextField livePointField;
 
-    private Stage stage;
+    @FXML
+    VBox vBox;
+
+    @FXML
+    Button toggleBtn;
 
     private NavigationManager navigationManager;
-
     private PersonViewModel viewModel;
 
     public void setNavigationManager(NavigationManager navigationManager) {
@@ -44,5 +54,24 @@ public class PersonViewController
         firstNameField.textProperty().bindBidirectional(viewModel.firstNameProperty());
         lastNameField.textProperty().bindBidirectional(viewModel.lastNameProperty());
         livePointField.textProperty().bindBidirectional(viewModel.livePointProperty(), new NumberStringConverter());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        vBox.setTranslateX(-200);
+
+        toggleBtn.setOnAction(event -> {
+            if (vBox.getTranslateX() < 0) {
+                // Ouvrir le panel
+                TranslateTransition openNav = new TranslateTransition(new Duration(350), vBox);
+                openNav.setToX(0);
+                openNav.play();
+            } else {
+                // Fermer le panel
+                TranslateTransition closeNav = new TranslateTransition(new Duration(350), vBox);
+                closeNav.setToX(-200);
+                closeNav.play();
+            }
+        });
     }
 }
