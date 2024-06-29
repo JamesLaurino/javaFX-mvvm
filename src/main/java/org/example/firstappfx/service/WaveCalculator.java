1fdc99e4ace3ea151821eed706fc7493effe982d
jamesLaurino <petersthomas90@gmail.com>
2024-06-29 13:29:53 +0200

    Calculation des valeurs pour le bench

---
+++ src/main/java/org/example/firstappfx/service/WaveCalculator.java
@@ -0,0 +1,36 @@
+package org.example.firstappfx.service;
+
+import javafx.beans.property.FloatProperty;
+import javafx.beans.property.SimpleFloatProperty;
+
+import java.util.List;
+
+public class WaveCalculator
+{
+    public List<FloatProperty> computeFiveWave(FloatProperty maxUser)
+    {
+        float maxUserFloat = maxUser.floatValue();
+        float firstVal = maxUserFloat * 0.65F;
+        float secondVal = maxUserFloat * 0.75F;
+        float thirdVal = maxUserFloat * 0.85F;
+        return List.of(new SimpleFloatProperty(firstVal),new SimpleFloatProperty(secondVal),new SimpleFloatProperty(thirdVal));
+    }
+    public List<FloatProperty> computeThirdWave(FloatProperty maxUser)
+    {
+        float maxUserFloat = maxUser.floatValue();
+        float firstVal = maxUserFloat * 0.70F;
+        float secondVal = maxUserFloat * 0.80F;
+        float thirdVal = maxUserFloat * 0.90F;
+        return List.of(new SimpleFloatProperty(firstVal),new SimpleFloatProperty(secondVal),new SimpleFloatProperty(thirdVal));
+    }
+    public List<FloatProperty> computeOneWave(FloatProperty maxUser)
+    {
+        float maxUserFloat = maxUser.floatValue();
+        float firstVal = maxUserFloat * 0.75F;
+        float secondVal = maxUserFloat * 0.85F;
+        float thirdVal = maxUserFloat * 0.95F;
+        return List.of(new SimpleFloatProperty(firstVal),new SimpleFloatProperty(secondVal),new SimpleFloatProperty(thirdVal));
+    }
+
+
+}
--- src/main/java/org/example/firstappfx/view/WendlerProgramViewController.java
+++ src/main/java/org/example/firstappfx/view/WendlerProgramViewController.java
@@ -1,10 +1,14 @@
 package org.example.firstappfx.view;
 
+import javafx.beans.property.FloatProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.util.converter.NumberStringConverter;
 import org.example.firstappfx.service.NavigationManager;
+import org.example.firstappfx.service.WaveCalculator;
 import org.example.firstappfx.viewmodel.WendlerProgramViewModel;
+
+import java.util.List;
 
 public class WendlerProgramViewController
 {
@@ -18,8 +22,27 @@ public class WendlerProgramViewController
     @FXML
     Label waveFiveThirdVal;
 
+    @FXML
+    Label waveThreeFirstVal;
+
+    @FXML
+    Label waveThreeSecondVal;
+
+    @FXML
+    Label  waveThreeThirdVal;
+
+    @FXML
+    Label waveOneFirstVal;
+
+    @FXML
+    Label waveOneSecondVal;
+
+    @FXML
+    Label waveOneThirdVal;
+
     private NavigationManager navigationManager;
     private WendlerProgramViewModel wendlerProgramViewModel;
+    private WaveCalculator waveCalculator = new WaveCalculator();
 
     public void setNavigationManager(NavigationManager navigationManager) {
         this.navigationManager = navigationManager;
@@ -28,13 +51,29 @@ public class WendlerProgramViewController
     public void setViewModel(WendlerProgramViewModel wendlerProgramViewModel)
     {
         this.wendlerProgramViewModel = wendlerProgramViewModel;
-        this.bindFields();
+        this.bindFieldsBenchWave();
     }
 
-    private void bindFields() {
-        waveFiveFirstVal.textProperty().bindBidirectional(wendlerProgramViewModel.getBenchMax(), new NumberStringConverter());
-        waveFiveSecondVal.textProperty().bindBidirectional(wendlerProgramViewModel.getBenchMax(), new NumberStringConverter());
-        waveFiveThirdVal.textProperty().bindBidirectional(wendlerProgramViewModel.getBenchMax(), new NumberStringConverter());
+    private void bindFieldsBenchWave() {
+
+        List<FloatProperty> wavePropertyList = waveCalculator.computeFiveWave(wendlerProgramViewModel.getBenchMax());
+        List<FloatProperty> waveThirdList = waveCalculator.computeThirdWave(wendlerProgramViewModel.getBenchMax());
+        List<FloatProperty> waveOneList = waveCalculator.computeOneWave(wendlerProgramViewModel.getBenchMax());
+
+        /* 5s BENCH */
+        waveFiveFirstVal.textProperty().bindBidirectional(wavePropertyList.get(0), new NumberStringConverter());
+        waveFiveSecondVal.textProperty().bindBidirectional(wavePropertyList.get(1), new NumberStringConverter());
+        waveFiveThirdVal.textProperty().bindBidirectional(wavePropertyList.get(2), new NumberStringConverter());
+
+        /* 3s BENCH */
+        waveThreeFirstVal.textProperty().bindBidirectional(waveThirdList.get(0), new NumberStringConverter());
+        waveThreeSecondVal.textProperty().bindBidirectional(waveThirdList.get(1), new NumberStringConverter());
+        waveThreeThirdVal.textProperty().bindBidirectional(waveThirdList.get(2), new NumberStringConverter());
+
+        /* 1s BENCH */
+        waveOneFirstVal.textProperty().bindBidirectional(waveOneList.get(0), new NumberStringConverter());
+        waveOneSecondVal.textProperty().bindBidirectional(waveOneList.get(1), new NumberStringConverter());
+        waveOneThirdVal.textProperty().bindBidirectional(waveOneList.get(2), new NumberStringConverter());
     }
 
     @FXML
